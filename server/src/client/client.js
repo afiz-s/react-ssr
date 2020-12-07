fd49c43e5105d43ac2896404e4e726f0caf3c14c
Afiz <afizshaik@gmail.com>
2020-12-07 20:17:56 +0530

    added redux for client

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import {BrowserRouter, Route} from 'react-router-dom'
 import Routes from './Routes'
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import {Provider} from 'react-redux'
+
+const store = createStore(reducers, {}, applyMiddleware(thunk))
 
 ReactDom.hydrate(
-    <BrowserRouter>
-        <Routes />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <Routes />
+        </BrowserRouter>
+    </Provider>
     , document.querySelector('#root'));
